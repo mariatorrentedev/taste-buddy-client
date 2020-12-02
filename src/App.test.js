2792d9b70f172fb67a1478c372a5d6515493bb80
María Torrente <caritotorrente@gmail.com>
2020-12-02 17:23:02 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+ReactDOM.render(<App />, document.getElementById("root"));
