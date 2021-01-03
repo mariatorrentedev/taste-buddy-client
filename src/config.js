900de170cd7c6df9dbdc18cf6bbe0da726ee32c0
María Torrente <caritotorrente@gmail.com>
2021-01-03 13:50:48 -0500

    fixing url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
   PORT: process.env.PORT || 8000,
-  API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8000/api",
+  API_BASE_URL:
+    process.env.API_BASE_URL || "https://taste-buddy-api.herokuapp.com/api",
   TOKEN_KEY: process.env.TOKEN_KEY || "taste-buddy-token",
 };
